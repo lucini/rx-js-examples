38984381593bda5403f2ce8ec8f28351fb82f4d1
rafael.lucini <rafael.lucini@viasoft.com.br>
2019-09-30 16:34:37 -0300

    TOPICO e mais

--- src/app/page/topico/topico.component.ts
+++ src/app/page/topico/topico.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {BehaviorSubject, Observable, of} from "rxjs";
+import {BehaviorSubject, Observable, of, Subject} from "rxjs";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-topico',
@@ -7,28 +8,34 @@ import {BehaviorSubject, Observable, of} from "rxjs";
   styleUrls: ['./topico.component.scss']
 })
 export class TopicoComponent implements OnInit, OnDestroy {
-  topico = new BehaviorSubject<string>("Texto Inicial");
+  topicoComComportamento = new BehaviorSubject<string>("Texto Inicial");
+  topico = new Subject<number>();
 
   ladoA: Observable<string>;
   ladoB: Observable<string>;
   ladoC: Observable<string>;
   ladoD: Observable<string>;
+  ladoDeFora: Observable<string>;
 
   constructor() {
+    this.topico.next(1);
+    this.topico.pipe(map(val => `VALOR: ${val}`)).subscribe(texto => this.ladoDeFora = of(texto));
   }
 
   ngOnDestroy(): void {
+    this.topicoComComportamento.unsubscribe();
     this.topico.unsubscribe();
   }
 
   ngOnInit(): void {
-    this.topico.asObservable().subscribe(value => this.ladoA = of(value));
-    this.topico.asObservable().subscribe(value => this.ladoB = of(value));
-    this.topico.asObservable().subscribe(value => this.ladoC = of(value));
-    this.topico.asObservable().subscribe(value => this.ladoD = of(value));
+    this.topicoComComportamento.asObservable().subscribe(value => this.ladoA = of(value));
+    this.topicoComComportamento.asObservable().subscribe(value => this.ladoB = of(value));
+    this.topicoComComportamento.asObservable().subscribe(value => this.ladoC = of(value));
+    this.topicoComComportamento.asObservable().subscribe(value => this.ladoD = of(value));
   }
 
   alterar(event): void {
-    this.topico.next(event.target.value);
+    this.topico.next(2);
+    this.topicoComComportamento.next(event.target.value);
   }
 }
